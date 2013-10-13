68b66531440b6f4d40730820dcc5d088eebcfa2a
Cadence Marseille <cadencemarseille@gmail.com>
2013-10-13 14:09:58 -0400

    Fix two copyright headers

--- src/gi/mod.rs
+++ src/gi/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The rust-object authors.
+// Copyright 2013 The rust-gobject authors.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
